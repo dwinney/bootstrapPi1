7f16a0bee437620c24ab17176efa1325a1226481
dwinney <daniel.winney@gmail.com>
2026-03-28 19:29:43 -0600

    process in terms of mod and arg no real and imag

--- root/process.C
+++ root/process.C
@@ -32,16 +32,16 @@ void process()
     std::unique_ptr<TTree> tree = std::make_unique<TTree>( "bootstrap", "bootstrap");
 
     // Temporary save locations for parameters
-    std::array<double,N_BIN_MAX-N_BIN_MIN+1> Nc, rNd, iNd;
+    std::array<double,N_BIN_MAX-N_BIN_MIN+1> Nc, mNd, aNd;
     double c, d, fcn;
 
     tree->Branch("fcn", &fcn, "fcn/D");
     for (int index = 0; index <= N_BIN_MAX-N_BIN_MIN; index++)
     {
         std::string si = std::to_string(index+N_BIN_MIN);
-        tree->Branch( (  "Nc_"+si+"").c_str(), &Nc[index],  (  "Nc_"+si+"/D").c_str());
-        tree->Branch( ("reNd_"+si+"").c_str(), &rNd[index], ("reNd_"+si+"/D").c_str());
-        tree->Branch( ("imNd_"+si+"").c_str(), &iNd[index], ("imNd_"+si+"/D").c_str());
+        tree->Branch( (   "Nc_"+si+"").c_str(), &Nc[index],  (  "Nc_"+si+"/D").c_str());
+        tree->Branch( ("modNd_"+si+"").c_str(), &mNd[index], ("modNd_"+si+"/D").c_str());
+        tree->Branch( ("argNd_"+si+"").c_str(), &aNd[index], ("argNd_"+si+"/D").c_str());
     };
     tree->Branch("c", &c, "c/D");
     tree->Branch("d", &d, "d/D");
@@ -70,7 +70,10 @@ void process()
 
             if (n <= N_BIN_MAX-N_BIN_MIN)
             {
-                is >> trash >> trash >> Nc[n] >> rNd[n] >> iNd[n];
+                double rNd, iNd;
+                is >> trash >> trash >> Nc[n] >> rNd >> iNd;
+                std::complex<double> Nd = rNd + I*iNd;
+                mNd[n] = abs(Nd); aNd[n] = arg(Nd);
                 n++;
                 continue;
             };
